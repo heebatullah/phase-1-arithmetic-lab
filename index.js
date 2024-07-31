5f441adec942ff3b9d0fc2b6a1c4063aaf1d2f8a
heeba <heebahassan182@gmail.com>
2024-07-31 20:04:06 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+multiply;
+
+let random = Math.floor(Math.random()*10) + 1;
+random;
+
+
+let num3 = 9 ;
+let num4 = 5 ;
+const mod = num3 % num4;
+mod;
+
+const max = Math.max (17,18,19,20);
+max;
